0bbb2348ac06827706c81691fa39cab5a339230d
gorkhali <gorkhali9999@gmail.com>
2020-09-05 11:10:50 +0545

    Updates

---
+++ precache-manifest.a18a939c9de60f70c535663658c3ae0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9160efeaf5d8c36d29907c69b6a2bfe4",
+    "url": "/food_recipe_react_app/index.html"
+  },
+  {
+    "revision": "324aaf9453364d2f293a",
+    "url": "/food_recipe_react_app/static/css/main.7ffc9699.chunk.css"
+  },
+  {
+    "revision": "9593b3fecb45747e31a5",
+    "url": "/food_recipe_react_app/static/js/2.c7c90c6c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/food_recipe_react_app/static/js/2.c7c90c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "324aaf9453364d2f293a",
+    "url": "/food_recipe_react_app/static/js/main.b4de667e.chunk.js"
+  },
+  {
+    "revision": "a3a482eddb913186a715",
+    "url": "/food_recipe_react_app/static/js/runtime-main.6aa2369f.js"
+  }
+]);
